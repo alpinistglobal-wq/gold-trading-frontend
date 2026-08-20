76b778b34b971407e34f4cd07e509958929056f2
alpinistglobal-wq <alpinist.global@gmail.com>
2026-08-21 03:20:19 +0500

    Add Home component with TradingDashboard

---
+++ app/page.js
@@ -0,0 +1,11 @@
+'use client';
+
+import TradingDashboard from '../components/TradingDashboard';
+
+export default function Home() {
+  return (
+    <main style={{ minHeight: '100vh', backgroundColor: '#090d16', padding: '20px' }}>
+      <TradingDashboard />
+    </main>
+  );
+}
